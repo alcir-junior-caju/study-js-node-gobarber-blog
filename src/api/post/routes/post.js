803423f0ee4e302c256714fb8089210f7b309422
Alcir Junior <junior@cajucomunica.com.br>
2022-06-11 15:46:17 -0300

    feat: Add Owner

--- src/api/post/routes/post.js
+++ src/api/post/routes/post.js
@@ -6,4 +6,42 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::post.post');
+// Modify these to match your Content-type
+const uid = 'api::post.post';
+const field = 'user';
+// Only modify these if the middleware or policy name is different
+const SetOwner = {
+    name: 'global::SetOwner',
+    config: {
+        field,
+        uid,
+    },
+};
+const IsOwner = {
+    name: 'global::IsOwner',
+    config: {
+        field,
+        uid,
+    },
+};
+
+module.exports = createCoreRouter(uid, {
+  config: {
+    create: {
+      middlewares: [SetOwner],
+    },
+    find: {
+      policies: [IsOwner],
+    },
+    findOne: {
+      policies: [IsOwner],
+    },
+    delete: {
+      policies: [IsOwner],
+    },
+    update: {
+      policies: [IsOwner],
+      middlewares: [SetOwner],
+    },
+  },
+});
