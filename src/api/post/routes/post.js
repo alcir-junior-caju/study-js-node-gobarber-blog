c80ed207812ccb0d291807d58a6080e654bd87ca
Alcir Junior <junior@cajucomunica.com.br>
2022-06-12 11:16:30 -0300

    feat: remove owner

--- src/api/post/routes/post.js
+++ src/api/post/routes/post.js
@@ -8,40 +8,40 @@ const { createCoreRouter } = require('@strapi/strapi').factories;
 
 // Modify these to match your Content-type
 const uid = 'api::post.post';
-const field = 'user';
-// Only modify these if the middleware or policy name is different
-const SetOwner = {
-    name: 'global::SetOwner',
-    config: {
-        field,
-        uid,
-    },
-};
-const IsOwner = {
-    name: 'global::IsOwner',
-    config: {
-        field,
-        uid,
-    },
-};
+// const field = 'user';
+// // Only modify these if the middleware or policy name is different
+// const SetOwner = {
+//     name: 'global::SetOwner',
+//     config: {
+//         field,
+//         uid,
+//     },
+// };
+// const IsOwner = {
+//     name: 'global::IsOwner',
+//     config: {
+//         field,
+//         uid,
+//     },
+// };
 
 module.exports = createCoreRouter(uid, {
-  config: {
-    create: {
-      middlewares: [SetOwner],
-    },
-    find: {
-      policies: [IsOwner],
-    },
-    findOne: {
-      policies: [IsOwner],
-    },
-    delete: {
-      policies: [IsOwner],
-    },
-    update: {
-      policies: [IsOwner],
-      middlewares: [SetOwner],
-    },
-  },
+  // config: {
+  //   create: {
+  //     middlewares: [SetOwner],
+  //   },
+  //   find: {
+  //     policies: [IsOwner],
+  //   },
+  //   findOne: {
+  //     policies: [IsOwner],
+  //   },
+  //   delete: {
+  //     policies: [IsOwner],
+  //   },
+  //   update: {
+  //     policies: [IsOwner],
+  //     middlewares: [SetOwner],
+  //   },
+  // },
 });
